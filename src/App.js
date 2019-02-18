444c7ded85c431cf5def569b75463de525791469
unknown <estebanbotero1036@gmail.com>
2019-02-18 17:19:48 -0500

    montaje 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Welcome from './Welcome'
+
+
 class App extends Component {
   constructor() {
     super();
@@ -13,8 +16,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        {this.state.names.map((name) =>
-          <Welcome name={name} />
+        {this.state.names.map((name, i) =>
+          <Welcome name={name} key={i}/>
         )}
       </div>
     );
